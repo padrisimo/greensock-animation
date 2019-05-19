22ac70834d5424e7d16d2a92cc8e69e88b7df979
padrisimo <adrianfernandezchamizo@gmail.com>
2019-05-20 01:29:57 +0200

    pause and resume animation

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-import { TweenMax } from "gsap"
+import { TweenMax, TimelineMax } from "gsap";
+const $ = (q) => document.querySelector(q);
 
 TweenMax.set("#box", {
   backgroundColor: "green",
@@ -8,8 +9,21 @@ TweenMax.set("#box", {
   y: "50px"
 })
 
-document.addEventListener("click", () => {
-  TweenMax.to("#box", 0.5, {
-    rotation: "+=30"
+
+const timeline = new TimelineMax({ repeat: -1 });
+
+timeline.pause()
+
+timeline.to("#box", 0.5, { x: 100 })
+timeline.to("#box", 0.5, { y: 100 })
+timeline.to("#box", 0.5, { x: 50 })
+timeline.to("#box", 0.5, { y: 50 })
+
+$("#box")
+  .addEventListener("click", () => {
+    if (timeline.isActive()) {
+      timeline.pause()
+    } else {
+      timeline.resume()
+    }
   })
-})
