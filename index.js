14104c6a84429bd208983ad60c7d15202d5a3c17
padrisimo <adrianfernandezchamizo@gmail.com>
2019-05-20 02:02:21 +0200

    control an array of elements with the same animation

--- index.js
+++ index.js
@@ -1,30 +1,25 @@
-import { TweenMax, TimelineMax } from "gsap";
-const $ = (q) => document.querySelector(q);
+import { TweenMax } from "gsap";
 
-TweenMax.set("#box", {
-  backgroundColor: "green",
-  width: "50px",
-  height: "50px",
-  x: "50px",
-  y: "50px"
+const divs = Array.from({ length: 100 }, () =>
+  document.createElement("div")
+)
+
+divs.forEach(div => {
+  TweenMax.set(div, {
+    position: "absolute",
+    x: `${Math.random() * window.innerWidth}px`,
+    y: `${Math.random() * window.innerHeight}px`,
+    width: 20,
+    height: 20,
+    backgroundColor: "green",
+    border: "3px solid black"
+  })
+
+  document.body.appendChild(div)
 })
-
-
-const timeline = new TimelineMax({ repeat: -1 });
-
-timeline.pause()
-
-timeline.to("#box", 0.5, { x: 100 })
-timeline.to("#box", 0.5, { y: 100 })
-timeline.to("#box", 0.5, { x: 50 })
-timeline.to("#box", 0.5, { y: 50 })
 
 document.addEventListener("click", event => {
   const { x, y } = event
-  TweenMax.fromTo(
-    "#box",
-    1,
-    { x, y },
-    { x: 500, y: 500 }
-  )
+
+  TweenMax.to(divs, 1, { x, y })
 })
